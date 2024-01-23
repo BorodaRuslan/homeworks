60cf1027e7d26ec08dfbdda93cba440cc11b82f9
Ruslan Boroda <kryskiv23@gmail.com>
2024-01-23 12:59:20 +0200

    Minor bugfixes

--- src/main/java/org/example/homework1/AccountService.java
+++ src/main/java/org/example/homework1/AccountService.java
@@ -5,68 +5,61 @@ import java.util.stream.Collectors;
 
 public class AccountService {
 
-    public static List<Account> getBalanceOver500(List<Account> accountsData){
+    public List<Account> getBalanceOver500(List<Account> accountsData){
         return accountsData.stream()
                 .filter(a -> a.getBalance() > 500.0)
                 .toList();
     }
-    public static Set<String> getCountry(List<Account> accountsData){
+    public Set<String> getCountry(List<Account> accountsData){
         return accountsData.stream()
                 .map(Account::getCountry)
                 .collect(Collectors.toSet());
 
     }
-    public static Boolean isYoungerThan2000(List<Account> accountsData){
+    public Boolean isYoungerThan2000(List<Account> accountsData){
         return accountsData.stream()
                 .anyMatch(a -> a.getBirthday().getYear() < 2000);
     }
-    public static Double sumOfBalancesForMaleAccounts(List<Account> accountsData){
+    public Double sumOfBalancesForMaleAccounts(List<Account> accountsData){
         return accountsData.stream()
-                .filter(a -> "Male".equals(a.getGender()))
+                .filter(a -> Gender.MALE.equals(a.getGender()))
                 .mapToDouble(Account::getBalance)
                 .sum();
     }
-    public static Map<Month, List<Account>> getGroupByBirthMonth(List<Account> accountsData){
+    public Map<Month, List<Account>> getGroupByBirthMonth(List<Account> accountsData){
         return accountsData.stream()
                 .collect(Collectors.groupingBy(a -> a.getBirthday().getMonth()));
     }
-    public static OptionalDouble getAverageBalanceInTheCountry(List<Account> accountsData, String country){
+    public Double calculateAverageBalanceForCountry(List<Account> accountsData, String country){
         return accountsData.stream()
                 .filter(a -> a.getCountry().equalsIgnoreCase(country))
                 .mapToDouble(Account::getBalance)
-                .average();
+                .average()
+                .orElse(Double.NaN);
     }
-    public static String getNames(List<Account> accountsData) {
+    public String getNames(List<Account> accountsData) {
         return accountsData.stream()
                 .map(a -> a.getFirstName() + " " + a.getLastName())
                 .collect(Collectors.joining(", "));
     }
 
-    public static List<Account> getSortedAccounts(List<Account> accountsData){
+    public List<Account> getSortedAccounts(List<Account> accountsData){
         return accountsData.stream()
                 .sorted(Comparator.comparing(Account::getLastName).thenComparing(Account::getFirstName))
                 .toList();
     }
 
-    public static Optional<Account> getOldestAccount(List<Account> accountsData){
+    public Optional<Account> getOldestAccount(List<Account> accountsData){
         return accountsData.stream()
                 .min(Comparator.comparing(Account::getBirthday));
     }
-    public static Map<Integer, Double> getAverageBalanceByBirthYear(List<Account> accountsData){
+    public Map<Integer, Double> getAverageBalanceByBirthYear(List<Account> accountsData){
         return accountsData.stream()
                 .collect(Collectors.groupingBy(a -> a.getBirthday().getYear(),
                         Collectors.averagingDouble(Account::getBalance)));
     }
-
-    public static Optional<Account> getLongestLastName(List<Account> accountsData){
+    public Optional<Account> getLongestLastName(List<Account> accountsData){
         return accountsData.stream()
                 .max(Comparator.comparingInt(a -> a.getLastName().length()));
     }
-
-
-
-
-
-
-
 }
